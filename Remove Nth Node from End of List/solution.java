367f3ff6d827f2369dfa182e06b394687a31359a
Anushka863 <dixitanushka68@gmail.com>
2026-08-20 22:21:21 +0530

    Add removeNthFromEnd method in Solution class
    Implement method to remove the Nth node from the end of a linked list.

--- Remove Nth Node from End of List/solution.java
+++ Remove Nth Node from End of List/solution.java
@@ -1,1 +1,36 @@
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
 
+        //calculate size
+        int sz=0;
+        ListNode temp=head;
+        while(temp!=null){
+            temp=temp.next;
+            sz++;
+        }
+
+        //agar mujhe piche se 5th elemnt ko hatana hai aur mera size bhi 5 hi hai
+
+        if(n==sz){
+            return head=head.next;
+            
+        }
+
+        //sz-n
+        int i=1;
+        int iToFind=sz-n;
+
+        ListNode prev=head;
+
+        while(i<iToFind){
+            prev=prev.next;
+            i++;
+        }
+        prev.next=prev.next.next;
+
+        return head;
+        
+
+        
+    }
+}
