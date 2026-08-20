5fc217479fe0993dbc77a0499748d684b1967e2d
Anushka863 <dixitanushka68@gmail.com>
2026-08-20 22:34:44 +0530

    Add subarray sum solution using prefix sums
    Implemented a solution to find the number of subarrays that sum to k using prefix sums and a hashmap.

--- Subarray sum equals k/solution.java
+++ Subarray sum equals k/solution.java
@@ -1,1 +1,28 @@
+import java.util.HashMap;
+class Solution{
+    public int subarraySum(int nums[],int k){
+        //using prefix_sum and hashmap
+        int prefix_sum_j=0;
+        int count=0;
 
+        //creating a hashmap
+        HashMap<Integer,Integer>map=new HashMap<>();
+        //initially sum is 0
+        map.put(0,1);//means 0 ek baar already exist karta hai
+        
+        for(int num:nums){
+            prefix_sum_j+=num;
+
+            //k=prefix_sum_j-prefix_sum_(i-1)
+            //prefix_sum_(i-1)=prefix_sum_j-k
+
+            if(map.containsKey(prefix_sum_j-k)){
+                count+=map.get(prefix_sum_j-k);
+            }
+            map.put(prefix_sum_j,map.getOrDefault(prefix_sum_j,0)+1);
+
+
+        }
+        return count;
+    }
+}
